b71f83da0a04ad078bd21595deddf61ea37d1825
Cheng Peng <chengpengyeah@126.com>
2019-01-07 11:06:41 +0800

    生成一个Frame窗口

---
+++ src/org/cp/tank/TankFrame.java
@@ -0,0 +1,15 @@
+package org.cp.tank;
+
+import java.awt.Frame;
+
+public class TankFrame {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setSize(800, 600);
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+	}
+
+}
